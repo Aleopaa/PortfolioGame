5a3ef20b26d775e6927ee4c7dcc944d70f10008d
Aleksandra Opacic <aleksandraopacic@hotmail.co.uk>
2024-12-05 13:48:54 +0000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { glslify } from "vite-plugin-glslify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), glslify()],
-  base: "./",
+  base: "/PortfolioGame/",
   build: {
     outDir: "dist", // Define the output directory
     assetsDir: "assets", // Specify the assets folder within the dist directory
